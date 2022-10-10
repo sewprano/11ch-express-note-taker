879fa61162d95597dda91a974f402441ceae0ce4
Deborah Wade <sopranosamurai@yahoo.com>
2022-10-10 14:38:15 -0700

    notesRouter work and the uuid generator

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const router = require('./routes');
+const apiRouter = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+//middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
@@ -12,13 +13,14 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.stastic('public'));
 
 //html pages
-
 //index
 app.get('*', (req, res) => {
     res.sendFile(path.join(_dirname, '/public/index.html'));
-})
-
+});
 //notes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
 
 //api routes
 
